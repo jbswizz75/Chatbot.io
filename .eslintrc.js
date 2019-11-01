6c418dd1359af317670ced610431ca65831bf162
jbswizz75 <jb.veesler@gmail.com>
2019-11-02 00:23:02 +0100

    chatbot config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,7 @@
 module.exports = {
-    extends: [
-        'airbnb-base',
-    ],
-    env: {
-        browser: true,
-        node: true,
-        es6: true,
-    },
-};
+    'extends': 'airbnb-base',
+    'env': {
+      'browser': true,
+      'node': true
+    }
+  };
