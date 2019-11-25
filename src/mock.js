6067443db62d3ae297cf85b77f11be73778b7f74
jbswizz75 <jb.veesler@gmail.com>
2019-11-25 12:58:39 +0100

    Update structure of the chatbot

---
+++ src/mock.js
@@ -0,0 +1,23 @@
+const sensa = [{
+  id: 'sansa-1',
+  name: 'Sansa',
+  avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/theory-1552510536.jpg?crop=0.501xw:1.00xh;0.0173xw,0&resize=480:*',
+  version: '1.1.1',
+  description: 'L\'hiver vient',
+  actions : [{
+    keyword : ['Salut', 'Hello', 'Bonjour'],
+    Response()
+  }]
+}];
+
+const tirion = [{
+    id: 'tirion-2',
+    name: 'Tirion',
+    avatar: 'https://avatarfiles.alphacoders.com/189/thumb-189599.jpg',
+    version: '1.1.1',
+    description: 'Un Lannister paye toujours ses dettes',
+    actions : [{
+      keyword : ['Salut', 'Hello', 'Bonjour'],
+      Response()
+    }]
+  }];
---
+++ src/tchat.js
@@ -0,0 +1,129 @@
+class Tchat {
+  /**
+   * render
+   */
+  render() {
+    const tchatMessages = document.querySelector('.tchat--messages');
+    const tchatContacts = document.querySelector('.tchat--contacts');
+    const item = {
+      id: 'sansa-123',
+      avatar: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/theory-1552510536.jpg?crop=0.501xw:1.00xh;0.0173xw,0&resize=480:*',
+      name: 'Sansa',
+      message: 'Bonjour',
+    };
+
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+    tchatMessages.innerHTML += this.tplMessageReceived(item);
+
+    tchatContacts.innerHTML += this.tplContact(item);
+
+    this.sendValue();
+  }
+
+  /**
+   * tpl contact
+   * @param {Object} item
+   * @param {String} item.id
+   * @param {String} item.avatar
+   * @param {String} item.name
+   * @param {String} item.message
+   */
+  tplContact(item) {
+    const { id, avatar, name } = item;
+    const count = document.querySelectorAll(`.${id}`).length;
+
+    return `
+      <li class="list-group-item d-flex justify-content-between align-items-center">
+        <img src="${avatar}" width="50" alt="avatar" class="rounded-circle">
+        <h5 class="display-5 mt-2">${name}</h5>
+        <span class="badge badge-primary badge-pill">${count}</span>
+      </li>`;
+  }
+
+  /**
+   * tpl message received
+   * @param {Object} item
+   * @param {String} item.id
+   * @param {String} item.avatar
+   * @param {String} item.name
+   * @param {String} item.message
+   */
+  tplMessageReceived(item) {
+    const date = new Date();
+    const {
+      avatar,
+      id,
+      name,
+      message,
+    } = item;
+
+    return `
+      <div class="${id} row mb-1">
+        <div class="col-6">
+          <div class="card">
+            <h5 class="card-header">
+              <img src="${avatar}" width="50" alt="avatar" class="rounded-circle">
+            </h5>
+            <div class="card-body">
+              <h5 class="card-title">${message}</h5>
+              <p class="card-text">${name} | ${date.toLocaleString()}</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-6"></div>
+      </div>`;
+  }
+
+  /**
+   * tpl message send
+   * @param {String} message
+   */
+  tplMessageSend(message) {
+    const date = new Date();
+    return `
+      <div class="row mb-1">
+        <div class="col-6"></div>
+        <div class="col-6">
+          <div class="card">
+            <h5 class="card-header">
+              <img src="https://png.pngtree.com/svg/20161212/f93e57629c.svg" width="50" alt="avatar" class="rounded-circle">
+            </h5>
+            <div class="card-body">
+              <h5 class="card-title">${message}</h5>
+              <p class="card-text">${date.toLocaleString()}</p>
+            </div>
+          </div>
+        </div>
+      </div>`;
+  }
+
+  /**
+   * send value
+   */
+  sendValue() {
+    const tchatMessages = document.querySelector('.tchat--messages');
+    const elBtn = document.querySelector('.tchat--button');
+
+    elBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      tchatMessages.innerHTML += this.tplMessageSend(this.getInputValue());
+    });
+  }
+
+  /**
+   * get input value
+   */
+  getInputValue() {
+    return document.querySelector('.tchat--input').value;
+  }
+}
+
+const tchat = new Tchat();
+
+tchat.render();
